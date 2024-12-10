ed03ac674a1d68f15c2ced1aa389ee49b8d2770d
cole92 <aleksandar9212@gmail.com>
2024-12-10 07:11:46 +0100

    Setup Firebase and added basic Register form

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 
