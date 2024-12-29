8e58969258eefce0389150958eabb3ffddbe6198
cole92 <aleksandar9212@gmail.com>
2024-12-29 09:44:51 +0100

    Added style.css for custom styles, simplified index.css and App.css, and applied form-container class to Login/Register forms for better styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-//import { useState } from 'react';
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { Navigate } from "react-router-dom";
