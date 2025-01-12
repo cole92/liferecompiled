f25fd349dff93925b3b5d3a8d232b164cfaf8dfb
cole92 <aleksandar9212@gmail.com>
2025-01-12 10:24:42 +0100

    Added firebase keys in env file

--- src/firebase.js
+++ src/firebase.js
@@ -7,17 +7,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDeAD5opPEZbTr5i4y6NExiKIlCqXVvf2E",
-  authDomain: "myblogapp-4bae3.firebaseapp.com",
-  projectId: "myblogapp-4bae3",
-  storageBucket: "myblogapp-4bae3.firebasestorage.app",
-  messagingSenderId: "357277833359",
-  appId: "1:357277833359:web:c2b6ca3c184a70815838fa"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-// Export funkcije za odjavu korisnika
+// Export funkcije za odjavu korisnika radi centralizacije logike autentifikacije
 export { signOut }; 
