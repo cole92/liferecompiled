45d56027848ee717c4a1a16034aa13f655167e3d
cole92 <aleksandar9212@gmail.com>
2025-02-20 07:17:43 +0100

    minor fixes

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,19 +1,25 @@
 import PropTypes from "prop-types";
-import "../styles/PostCard.css"
+import { useNavigate } from "react-router-dom";
+import "../styles/PostCard.css";
 
 const PostCard = ({ post }) => {
   const { title, description, createdAt, tags, author, comments } = post;
 
+  const navigate = useNavigate();
+
   return (
-    <div className="post-card">
+    <div
+      className="post-card"
+      onClick={() => navigate(`/post/${post.id}`)}
+      style={{ cursor: "pointer" }}
+    >
       {/* Profilna slika i ime autora */}
       <div className="post-author">
-        
-        <img src={author.profilePicture}  alt="Author"/>
+        <img src={author.profilePicture} alt="Author" />
         <span>{author?.name || "Unknown"}</span>
       </div>
       
-
+      {/* Naslov, opis i datum objave */}
       <h2 className="post-title">{title}</h2>
       <p className="post-description">{description}</p>
       <span className="post-date">
@@ -28,20 +34,36 @@ const PostCard = ({ post }) => {
           </span>
         ))}
       </div>
-        {/* Broj komentara */}
-        
+      {/* Broj komentara */}
+      <p className="post-comments">{comments?.length || 0} comments</p>
 
+      {/* Prikaz 1-2 komentara ako ih ima */}
+      {comments?.slice(0, 2).map((comment, index) => (
+        <p key={index} className="post-comment">
+          &quot;{comment.text}
+        </p>
+      ))}
     </div>
   );
 };
 
 PostCard.propTypes = {
   post: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired, // Firebase ID posta
+    title: PropTypes.string.isRequired, // Naslov posta
+    description: PropTypes.string.isRequired, // Opis posta
     createdAt: PropTypes.object.isRequired, // Firestore Timestamp
     tags: PropTypes.arrayOf(PropTypes.shape({ text: PropTypes.string }))
-      .isRequired,
+      .isRequired, // Tagovi
+    author: PropTypes.shape({
+      name: PropTypes.string.isRequired, // Ime autora
+      profilePicture: PropTypes.string.isRequired, // URL slike autora
+    }).isRequired, // Autor je objekat i obavezno mora imati ove vrednosti
+    comments: PropTypes.arrayOf(
+      PropTypes.shape({
+        text: PropTypes.string.isRequired, // Tekst komentara
+      })
+    ).isRequired, // Komentari su niz objekata sa tekstom
   }).isRequired,
 };
 
