301381a5f815a07d54410e983023bdd55e1f78f5
cole92 <aleksandar9212@gmail.com>
2025-05-17 08:32:21 +0200

    feat: render current users posts in MyPosts with loading and empty state

--- src/pages/MyPosts.jsx
+++ src/pages/MyPosts.jsx
@@ -1,12 +1,49 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Spinner from "../components/Spinner";
+import EmptyState from "./dashboard/components/EmptyState";
+import { useEffect } from "react";
+import { collection, query, where, orderBy, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import PostsList from "../components/PostsList";
 
 // Dashboard komponenta za prikaz podataka korisnika
 const MyPosts = () => {
-  // Preuzimamo stanja i metode iz AuthContext-a
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] =  useState(true);
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const postRef = collection(db, "posts");
+
+        const q = query(
+          postRef,
+          where("userId", "==", user.uid),
+          orderBy("createdAt", "desc")
+        );
+        const querySnapshot = await getDocs(q);
+
+        const userPosts = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+
+        setPosts(userPosts);
+      } catch (error) {
+        console.error("Error fetching posts:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (user?.uid) {
+      fetchPost();
+    }
+  }, [user?.uid]);
 
   // Prikaz korisnickog interfejsa
   return (
@@ -20,6 +57,15 @@ const MyPosts = () => {
       >
         Create New Post
       </button>
+
+      {/* Loading state */}
+      {isLoading && <Spinner message="Loading your posts..." />}
+
+      {/* Empty state */}
+      {!isLoading && posts.length === 0 && (
+        <EmptyState message="You haven't created any posts yet." />
+      )}
+      {!isLoading && posts.length > 0 && <PostsList posts={posts} />}
     </div>
   );
 };
