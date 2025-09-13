63afda517f5e7b5fcb9233d5b999a115e7864fd9
cole92 <aleksandar9212@gmail.com>
2025-09-13 10:12:33 +0200

    chore: ESLint flat config + deps; ignore .vscode

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ['dist'] },
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
