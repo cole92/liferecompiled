17019fb6c31dc41e3683e66fc109267f34a77c4e
cole92 <aleksandar9212@gmail.com>
2026-02-27 07:43:10 +0100

    chore(functions): use env vars for Cloudinary config

--- functions/cloudinary.js
+++ functions/cloudinary.js
@@ -1,7 +1,24 @@
-/* eslint-disable no-undef */
-const functions = require("firebase-functions");
+/* eslint-env node */
+/* global require, process, module */
 const cloudinary = require("cloudinary").v2;
 
-cloudinary.config(functions.config().cloudinary);
+const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+const missing = [];
+if (!cloudName) missing.push("CLOUDINARY_CLOUD_NAME");
+if (!apiKey) missing.push("CLOUDINARY_API_KEY");
+if (!apiSecret) missing.push("CLOUDINARY_API_SECRET");
+
+if (missing.length) {
+  throw new Error(`[CF] Missing Cloudinary env vars: ${missing.join(", ")}`);
+}
+
+cloudinary.config({
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
+});
 
 module.exports = cloudinary;
